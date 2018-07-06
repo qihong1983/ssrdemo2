4bd9ec2c03cb0282a0596d55e42e44c0a6ea3375
qihong1 <qihong1@lenovo.com>
2018-07-06 13:54:24 +0800

    aaaaa

---
+++ static/service-worker.js
@@ -0,0 +1,50 @@
+//importScripts(
+ // "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js"
+//);
+importScripts(
+  "/workbox-sw.js"
+);
+
+
+
+//importScripts('/workbox-sw.prod.v2.1.3.js');
+
+
+//workbox.precache(['./']);
+
+//workbox.precaching.precache(['./']);
+
+// cache HTML
+ workbox.routing.registerRoute(
+   new RegExp('(.*)'),
+   workbox.strategies.staleWhileRevalidate({
+    cacheName: "html-content"
+   })
+  );
+
+  // cache bundles
+  workbox.routing.registerRoute(
+    new RegExp("/_next/(.*)"),
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "bundled-content"
+    })
+  );
+  
+  // cache images
+  workbox.routing.registerRoute(
+    new RegExp("/static/(.*)"),
+    workbox.strategies.cacheFirst({
+      cacheName: "images",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+  
+  // offline analytics
+  workbox.googleAnalytics.initialize();
+
+
