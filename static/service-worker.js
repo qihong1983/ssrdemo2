bfc99ac7d9e34c5a95ce6262b16ed93b4da1d3e8
qihong1 <qihong1@lenovo.com>
2018-07-08 17:45:12 +0800

    fffdf

--- static/service-worker.js
+++ static/service-worker.js
@@ -7,74 +7,44 @@ importScripts(
 
 
 
-
-// workbox.precaching.precache(['./','./about/']);
-
-//  workbox.routing.registerRoute(
-//     new RegExp("/mock/5a2dca93e9ee5f7c09d8c6d7/(.*)"),
-//    workbox.strategies.networkFirst()
-// );
-
-//  workbox.routing.registerRoute(
-//    new RegExp('./$'),
-//    workbox.strategies.staleWhileRevalidate({
-//     cacheName: "html-content"
-//    })
-//   );
-
-//   workbox.routing.registerRoute(
-//     new RegExp("/_next/(.*)"),
-//     workbox.strategies.staleWhileRevalidate({
-//       cacheName: "bundled-content"
-//     })
-//   );
+//importScripts('/workbox-sw.prod.v2.1.3.js');
 
 
+//workbox.precache(['./']);
 
- 
-//   workbox.routing.registerRoute(
-//     new RegExp("/static/(.*)"),
-//     workbox.strategies.cacheFirst({
-//       cacheName: "images",
-//       plugins: [
-//         new workbox.expiration.Plugin({
-//           maxEntries: 60,
-//           maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-//         })
-//       ]
-//     })
-//   );
+//workbox.precaching.precache(['./']);
+
+// cache HTML
+ workbox.routing.registerRoute(
+   new RegExp('./(.*)'),
+   workbox.strategies.staleWhileRevalidate({
+    cacheName: "html-content"
+   })
+  );
+
+  // cache bundles
+  // workbox.routing.registerRoute(
+  //   new RegExp("/_next/(.*)"),
+  //   workbox.strategies.staleWhileRevalidate({
+  //     cacheName: "bundled-content"
+  //   })
+  // );
   
-//   workbox.googleAnalytics.initialize();
+  // cache images
+  // workbox.routing.registerRoute(
+  //   new RegExp("/static/(.*)"),
+  //   workbox.strategies.cacheFirst({
+  //     cacheName: "images",
+  //     plugins: [
+  //       new workbox.expiration.Plugin({
+  //         maxEntries: 60,
+  //         maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+  //       })
+  //     ]
+  //   })
+  // );
+  
+  // offline analytics
+  workbox.googleAnalytics.initialize();
 
 
-
-// const staticAssets = [
-//   './'
-// ];
-
-
-// const wb = new WorkboxSW();
-
-// wb.precache(staticAssets);
-
-// wb.router.registerRoute('https://www.easy-mock.com/(.*)', wb.strategies.networkFirst());
-
-
-//   wb.router.registerRoute(
-//     new RegExp("/_next/(.*)"),
-//     wb.strategies.staleWhileRevalidate({
-//       cacheName: "bundled-content"
-//     })
-//   );
-
-// wb.router.registerRoute(/.*\.(png|jpg|jpeg|gif)/, wb.strategies.cacheFirst({
-//   cacheName: 'news-images',
-//   cacheExpiration: {
-//     maxEntries: 2,
-//     maxAgeSeconds: 7 * 24 * 60 * 60,
-//   },
-//   cacheableResponse: {
-//     statuses: [0, 200]
-//   },
-// }));
