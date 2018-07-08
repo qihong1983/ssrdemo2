83dba8b2e5b7a15a96b429690907a03f4579d53f
qihong1 <qihong1@lenovo.com>
2018-07-08 17:36:59 +0800

    asdfasdfasdf

---
+++ out/service-worker.js
@@ -0,0 +1,68 @@
+//importScripts(
+ // "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js"
+//);
+importScripts(
+  "/workbox-sw.js"
+);
+
+
+
+//importScripts('/workbox-sw.prod.v2.1.3.js');
+
+
+//workbox.precache(['./']);
+
+workbox.precaching.precache(['./','./about/']);
+
+ workbox.routing.registerRoute(
+    new RegExp("/mock/5a2dca93e9ee5f7c09d8c6d7/(.*)"),
+   workbox.strategies.networkFirst()
+);
+
+// cache HTML
+ workbox.routing.registerRoute(
+   new RegExp('./$'),
+   workbox.strategies.staleWhileRevalidate({
+    cacheName: "html-content"
+   })
+  );
+
+  // cache bundles
+  workbox.routing.registerRoute(
+    new RegExp("/_next/(.*)"),
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "bundled-content"
+    })
+  );
+
+  // cache bundles
+  // workbox.routing.registerRoute(
+  //   new RegExp("https://www.easy-mock.com/(.*)"),
+  //   workbox.strategies.staleWhileRevalidate({
+  //     cacheName: "fetch-content"
+  //   })
+  // );
+
+ 
+
+  // https://www.easy-mock.com/mock/5a2dca93e9ee5f7c09d8c6d7/Aaa/nextDemoTables
+  // workbox.routing.registerRoute('https://www.easy-mock.com/(.*)',  workbox.strategies.networkFirst());  
+  
+  // cache images
+  workbox.routing.registerRoute(
+    new RegExp("/static/(.*)"),
+    workbox.strategies.cacheFirst({
+      cacheName: "images",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+  
+  // offline analytics
+  workbox.googleAnalytics.initialize();
+
+
--- static/service-worker.js
+++ static/service-worker.js
@@ -7,62 +7,74 @@ importScripts(
 
 
 
-//importScripts('/workbox-sw.prod.v2.1.3.js');
+
+// workbox.precaching.precache(['./','./about/']);
+
+//  workbox.routing.registerRoute(
+//     new RegExp("/mock/5a2dca93e9ee5f7c09d8c6d7/(.*)"),
+//    workbox.strategies.networkFirst()
+// );
+
+//  workbox.routing.registerRoute(
+//    new RegExp('./$'),
+//    workbox.strategies.staleWhileRevalidate({
+//     cacheName: "html-content"
+//    })
+//   );
+
+//   workbox.routing.registerRoute(
+//     new RegExp("/_next/(.*)"),
+//     workbox.strategies.staleWhileRevalidate({
+//       cacheName: "bundled-content"
+//     })
+//   );
 
 
-//workbox.precache(['./']);
 
-workbox.precaching.precache(['./','./about/']);
+ 
+//   workbox.routing.registerRoute(
+//     new RegExp("/static/(.*)"),
+//     workbox.strategies.cacheFirst({
+//       cacheName: "images",
+//       plugins: [
+//         new workbox.expiration.Plugin({
+//           maxEntries: 60,
+//           maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+//         })
+//       ]
+//     })
+//   );
+  
+//   workbox.googleAnalytics.initialize();
 
- workbox.routing.registerRoute(
-    new RegExp("/mock/5a2dca93e9ee5f7c09d8c6d7/(.*)"),
-   workbox.strategies.networkFirst()
-);
 
-// cache HTML
- workbox.routing.registerRoute(
-   new RegExp('./$'),
-   workbox.strategies.staleWhileRevalidate({
-    cacheName: "html-content"
-   })
-  );
 
-  // cache bundles
-  workbox.routing.registerRoute(
+const staticAssets = [
+  './'
+];
+
+
+const wb = new WorkboxSW();
+
+wb.precache(staticAssets);
+
+wb.router.registerRoute('https://www.easy-mock.com/(.*)', wb.strategies.networkFirst());
+
+
+  wb.router.registerRoute(
     new RegExp("/_next/(.*)"),
-    workbox.strategies.staleWhileRevalidate({
+    wb.strategies.staleWhileRevalidate({
       cacheName: "bundled-content"
     })
   );
 
-  // cache bundles
-  // workbox.routing.registerRoute(
-  //   new RegExp("https://www.easy-mock.com/(.*)"),
-  //   workbox.strategies.staleWhileRevalidate({
-  //     cacheName: "fetch-content"
-  //   })
-  // );
-
- 
-
-  // https://www.easy-mock.com/mock/5a2dca93e9ee5f7c09d8c6d7/Aaa/nextDemoTables
-  // workbox.routing.registerRoute('https://www.easy-mock.com/(.*)',  workbox.strategies.networkFirst());  
-  
-  // cache images
-  workbox.routing.registerRoute(
-    new RegExp("/static/(.*)"),
-    workbox.strategies.cacheFirst({
-      cacheName: "images",
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-        })
-      ]
-    })
-  );
-  
-  // offline analytics
-  workbox.googleAnalytics.initialize();
-
-
+wb.router.registerRoute(/.*\.(png|jpg|jpeg|gif)/, wb.strategies.cacheFirst({
+  cacheName: 'news-images',
+  cacheExpiration: {
+    maxEntries: 2,
+    maxAgeSeconds: 7 * 24 * 60 * 60,
+  },
+  cacheableResponse: {
+    statuses: [0, 200]
+  },
+}));
